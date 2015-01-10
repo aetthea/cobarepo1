5feb99920271dd50296b7a94d7f837ec7a263ff3
arifadli84 <ari.tugas@windowslive.com>
2015-01-10 13:45:08 -0800

    Rubah Mahasiswa

--- app/model/mahasiswa.java
+++ app/model/mahasiswa.java
@@ -1,5 +1,32 @@
-package model;
+package models;
+import java.util.Date;
 
-public class mahasiswa {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
+import play.db.jpa.Model;
+
+//import play.data.validation.Required;
+//import play.db.jpa.Model;
+
+@Entity
+public class Anggota extends Model {
+	//@Required
+	public String nama;
+	//@Required
+	public String alamat;
+	//@Required
+	public String pendidikan;
+	public Date tgllhr;
+    public String kodepos;
+    @ManyToOne
+    public JenisKelamin kelamin;
+    
+    public void setNama(String nama){
+      this.nama=nama;
+    }
+	public String toString() {
+	      return nama;
+	   }
+	
 }
